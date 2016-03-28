087446a5e1a51804d003ea205dbc27851de7da5a
yinzishao <370953598@qq.com>
2016-03-28 14:09:57 +0800

    用apache部署在本机上

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -105,12 +105,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+#
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+#     ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),
+#     ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
+#     ('images',os.path.join(STATIC_ROOT,'images').replace('\\','/') ),
+#     ('upload',os.path.join(STATIC_ROOT,'upload').replace('\\','/') ),
+# )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'collected_static')
 STATICFILES_DIRS = (
-    ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),
-    ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
-    ('images',os.path.join(STATIC_ROOT,'images').replace('\\','/') ),
-    ('upload',os.path.join(STATIC_ROOT,'upload').replace('\\','/') ),
+    os.path.join(BASE_DIR, "common_static"),
+    '/path/to/others/static/',
 )
